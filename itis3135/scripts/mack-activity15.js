4228ce6942fbf052c1bfe44052eb9641ecf8be72
tdninja1 <jmack23@uncc.edu>
2021-11-24 13:08:50 -0500

    Finished activity 15 completely

--- itis3135/scripts/mack-activity15.js
+++ itis3135/scripts/mack-activity15.js
@@ -1,3 +1,40 @@
-$(document).ready(function(){
+/*
+{"facultymembers":[
+	{
+		"full_name": "Debarati Basu",
+		"first_name": "Debarati",
+		"image": "faculty/Dr.Basu.png",
+		"department": "Software and Information Systems",
+		"bio": "Assistant Teaching Professor"
+	},
+*/
 
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "json_files/facultyList.json", //CHANGED FROM "xml" TO "json"
+        beforeSend: function() {
+            $("#faculty").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json", //CHANGED FROM "xml" TO "json"
+        success: function(data) {
+            $("#faculty").html("");
+        
+            //JSON UNDER
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $('#faculty').append(
+                        "<img src=' " + value.image + " '> </img>" +
+                        '<h2>' + value.full_name + '</h2>' + 
+                        '<h3>' + value.department + '</h3>' +
+                        '<p>' + value.bio + '</p>'
+                    );
+                });
+            });
+        }
+    });
 });
