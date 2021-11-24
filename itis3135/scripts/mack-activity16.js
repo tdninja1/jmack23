ecede981de832f92bbc4e4a507a28c35c9997ecc
tdninja1 <jmack23@uncc.edu>
2021-11-24 14:37:31 -0500

    Finished activity 16

--- itis3135/scripts/mack-activity16.js
+++ itis3135/scripts/mack-activity16.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?" 
+        + "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+
+        $.getJSON(url, function(data) {
+            var html = "";
+
+            $.each(data.items, function(i, item) {
+                // html += "<h3>" + item.title + "</h3>";
+                // html += item.description; //displays photo, and descriptive text for the photo
+                // html += item.media.m; //photo url
+
+                html += "<img src=" + item.media.m + ">"; //holds collection of urls for all photos
+               
+
+                //Remove first paragraph of the description
+                // html = html.replace
+                // (//start html.replace
+                //      "<p><a href=\ " //add an image element
+                //     + 
+                //     'http://www.flickr.com/people/' 
+                //      +
+                //      "82407828@N07/\"zakruvalcaba<\/a>" 
+                //     +
+                //     " posted a photo:<\/p>", ""
+                //     )//end html.replace
+                
+            });
+            $("#new_building").html(html);
+            
+
+        });
 
 });
