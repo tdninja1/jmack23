203d39a1559f6698715fc9f136e4f5bad96a247a
tdninja1 <jmack23@uncc.edu>
2021-12-05 14:37:29 -0500

    Home page ajax request

--- itis3135/assignment/scripts/angiesdelights.js
+++ itis3135/assignment/scripts/angiesdelights.js
@@ -17,7 +17,7 @@
 $(document).ready(function() {
     $.ajax({
         type: "get",
-        url: "json_files/generated.json", //CHANGED FROM "xml" TO "json"
+        url: "json_files/favProducts.json", //CHANGED FROM "xml" TO "json"
         beforeSend: function() {
             $("#fav_products").html("Loading...");
         },
@@ -32,9 +32,47 @@ $(document).ready(function() {
             //JSON UNDER
             $.each(data, function() {
                 $.each(this, function(key, value) {
-                    $('#faculty').append(
+                    $('#fav_products').append(
                         "<img src=' " + value.image + " '> </img>" +
                         '<h2>' + value.name + '</h2>' + 
+                        '<p>' + value.description + '</p>'
+                    );
+                });
+            });
+        }
+    });
+});
+
+//AJAX REQUEST HOME PAGE
+/*
+{
+  "home": [
+    {
+      "description": "Welcome Angie's Delights. Here, you can find anything and everything bakery related that we have listed. Only the most delightful products are listed on our page. Enjoy!"
+    }
+  ]
+}
+
+*/
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "json_files/home.json", //CHANGED FROM "xml" TO "json"
+        beforeSend: function() {
+            $("#home").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json", //CHANGED FROM "xml" TO "json"
+        success: function(data) {
+            $("#home").html("");
+        
+            //JSON UNDER
+            $.each(data, function() {
+                $.each(this, function(key, value) {
+                    $('#home').append(
                         '<p>' + value.description + '</p>'
                     );
                 });
