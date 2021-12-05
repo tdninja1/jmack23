00ea5fb8bed55ad3c4a1ef8b4dad775eb73ccaaf
tdninja1 <jmack23@uncc.edu>
2021-12-05 15:25:43 -0500

    Update to js files

--- itis3135/assignment/scripts/angiesdelights.js
+++ itis3135/assignment/scripts/angiesdelights.js
@@ -5,7 +5,6 @@
 3.) Random Item Picker from 10 listed items
 */
 //AJAX FAVORITE PRODUCTS
-
 /*
 {"favproducts":[
 	{
@@ -43,45 +42,6 @@ $(document).ready(function() {
         }
     });
 });
-
-//AJAX REQUEST HOME PAGE
-/*
-{
-  "home": [
-    {
-      "description": "Welcome Angie's Delights. Here, you can find anything and everything bakery related that we have listed. Only the most delightful products are listed on our page. Enjoy!"
-    }
-  ]
-}
-
-*/
-$(document).ready(function() {
-    $.ajax({
-        type: "get",
-        url: "json_files/home.json", //CHANGED FROM "xml" TO "json"
-        beforeSend: function() {
-            $("#home").html("Loading...");
-        },
-        timeout: 10000,
-        error: function(xhr, status, error) {
-            alert("Error: " + xhr.status + " - " + error);
-        },
-        dataType: "json", //CHANGED FROM "xml" TO "json"
-        success: function(data) {
-            $("#home").html("");
-        
-            //JSON UNDER
-            $.each(data, function() {
-                $.each(this, function(key, value) {
-                    $('#home').append(
-                        '<p>' + value.description + '</p>'
-                    );
-                });
-            });
-        }
-    });
-});
-
 
 
 //Image Slideshow works with bxSlider plugin - JQUERY PLUGIN
